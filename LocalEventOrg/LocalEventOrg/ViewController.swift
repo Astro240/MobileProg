0a2412fff2a310c5cd29f38f6a047c384c90f599
Astro240 <Abdullaj240@gmail.com>
2024-11-25 12:35:56 +0300

    First commit

---
+++ LocalEventOrg/LocalEventOrg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalEventOrg
+//
+//  Created by BP-36-201-06 on 25/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
