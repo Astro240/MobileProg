4f2cdec7452472fb88f2e6ffdd8253ad65332e1b
Alhasan223 <alhasan223@gmail.com>
2024-11-28 13:42:31 +0300

    pp

---
+++ LocalEventOrg/LocalEventOrg/LogInTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LogInTableViewController.swift
+//  LocalEventOrg
+//
+//  Created by BP-36-201-05 on 28/11/2024.
+//
+
+import UIKit
+
+class LogInTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
