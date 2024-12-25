1268db34b9f6bbfa3ba94679e762401ac76a9732
Astro240 <abdullaj240@gmail.com>
2024-12-25 07:36:20 -0800

    i want to die

--- LocalEventOrg/LocalEventOrg/Database.swift
+++ LocalEventOrg/LocalEventOrg/Database.swift
@@ -19,38 +19,27 @@ func populateDatabase(){
 
 func populateEvents() {
     let ref = Database.database().reference()
-
     ref.child("Events").observeSingleEvent(of: .value, with: { (snapshot) in
         if let eventsDict = snapshot.value as? [String: Any] {
             for (eventID, eventData) in eventsDict {
                 if let eventDetails = eventData as? [String: Any],
                    let eventName = eventDetails["Name"] as? String,
-                   let eventDescription = eventDetails["Description"] as? String,
-                   let ticketsDict = eventDetails["Tickets"] as? [String: Any] {
-
+                   let eventDate = eventDetails["Date"] as? String,
+                   let eventLocation = eventDetails["Location"] as? String,
+                   let eventCategories = eventDetails["Categories"] as? [String] {
+                    Item.promotedApps.append(.app(App(promotedHeadline: "Recommended For You", title: eventName, subtitle: "", price: 3.99,color:UIImage(named: "ComicCon"))))
                     print("Event ID: \(eventID)")
                     print("Event Name: \(eventName)")
-                    print("Description: \(eventDescription)")
-                    Item.promotedApps.append(.app(App(promotedHeadline: "Recommended For You", title: eventName, subtitle: "", price: 3.99,color:UIImage(named: "ComicCon"))))
-                    // Loop through tickets and print ticket details
-                    for (ticketID, ticketData) in ticketsDict {
-                        if let ticketDetails = ticketData as? [String: Any],
-                           let ticketName = ticketDetails["Name"] as? String,
-                           let ticketPrice = ticketDetails["Price"] as? Double,
-                           let ticketBenefits = ticketDetails["Benefits"] as? String {
-                            print("  Ticket ID: \(ticketID)")
-                            print("  Ticket Name: \(ticketName)")
-                            print("  Price: \(ticketPrice)")
-                            print("  Benefits: \(ticketBenefits.isEmpty ? "None" : ticketBenefits)")
-                        }
-                    }
+                    print("Date: \(eventDate)")
+                    print("Location: \(eventLocation)")
+                    print("Categories: \(eventCategories.joined(separator: ", "))")
                     print("\n") // New line for better readability
                 }
             }
-            // Return the populated items after the database fetch is complete
         } else {
             print("No data found")
         }
+    }, withCancel: { error in
+        print("Error fetching data: (error.localizedDescription)")
     })
 }
-
